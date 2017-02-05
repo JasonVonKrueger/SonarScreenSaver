877422e56742103c4583e4525190887d7e1b1208
JasonVonKrueger <rpoore@gmail.com>
2017-02-05 10:26:48 -0500

    Update ScreenSaverTrigger.ino

--- ScreenSaverTrigger/ScreenSaverTrigger.ino
+++ ScreenSaverTrigger/ScreenSaverTrigger.ino
@@ -7,17 +7,37 @@
 #define TRIGGER_PIN  9  
 #define ECHO_PIN     8  
 #define MAX_SONAR_DISTANCE 500 
+#define DELAY 500
+#define SECONDS_TO_TRIGGER 3
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_SONAR_DISTANCE);
 
+int _counter = 0;
+int _max_ping_distance = 30;
+
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);     
 }
 
 void loop() {
-  delay(1000);
+  delay(DELAY);
   unsigned int uS = sonar.ping(); 
   int dist = uS / US_ROUNDTRIP_IN;
 
-  Serial.println(dist);
+  // let the user set _max_ping_distance
+  if (Serial.available() > 0) {
+          String incomingByte = Serial.readString();
+          _max_ping_distance = incomingByte.toInt();
+  }  
+  
+  // if dist has been more than _max_ping_distance for 3 seconds, trigger the screen saver
+  if (dist >= _max_ping_distance)
+    _counter++;
+  else
+    _counter = 0;
+
+  // delay is 500ms so multiply times 2 for seconds
+  if (_counter >= (SECONDS_TO_TRIGGER * 2))
+    Serial.println("TRIG");
+
 }
